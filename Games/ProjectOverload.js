2888c52bbb6a21bb59ff8fcb85ceece31cf158d3
Jamison Smith <102773780+Jamison-smith@users.noreply.github.com>
2023-06-16 13:38:52 -0700

    Update ProjectOverload.js

--- Games/ProjectOverload.js
+++ Games/ProjectOverload.js
@@ -1,8 +1,4 @@
 function commands(command) {
-  if (command.includes('Page_Load_Command ')) {
-    var t8 = comamnd.split("Page_Load_Command Command: ")
-    localStorage.setItem("Page_Load_Command", t8[1])
-  } else {
   if (command.includes('SetCookie ')) {
     var t = command.split("SetCookie ")
     cookies=t[1]
@@ -74,6 +70,30 @@ function commands(command) {
                       OVERFLOWdia.open = false
                     }
                   } else {
+                    if (command.includes('Custom_Info_Dia ')) {
+                      var c8 = command.split('Custom_Info_Dia ButtonGroup: ')
+                      if (c8[1].includes(' 1 ') {
+                        InfoYesButtonGroup.removeAttribute('hidden')
+                        var c9 = command.split(' Head: ')
+                        Head_Text2.textContent = c9[1]
+                        Modifyable_info_dia.open = true
+                      }
+                      var t9 = command.split(' Head: ')
+                      Head_Text2.textContent = t9[1]
+                      Modifyable_info_dia.open = true
+                    } else {
+                        if (command.includes('Return ') {
+                          var c10 = command.split(' ReturnCode: ')
+                          var c11 = command.split(' CommandRunAfter:  ')
+                          if (c10==0) {
+                            var t11 = commands("Custom_Info_Dia ButtonGroup: 1 Head: Are you sure?")
+                            if (InfoButtonsPressed=="yes") {
+                              commands(c11[1])
+                            }
+                          } else {
+                            return 1
+                          }
+                        } else {
                       errorsfx.play()
                       OVERFLOWdiaError.open = true
                     }
@@ -83,4 +103,6 @@ function commands(command) {
             }            
           }
         }
-      }
+    }
+}
+
