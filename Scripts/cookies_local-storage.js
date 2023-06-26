12fbfde216a67c66d520af42939ae2f4fc2a8cb0
Jamison Smith <102773780+Jamison-smith@users.noreply.github.com>
2023-06-25 21:30:37 -0700

    Update cookies_local-storage.js

--- Scripts/cookies_local-storage.js
+++ Scripts/cookies_local-storage.js
@@ -1,1 +1,14 @@
+var AccessedPages;
 
+var Access_Check;
+
+Access_Check.CheckAccess = function(pageid) {
+  if (pageid=="s$"+100) {
+    var check_start = localStorage.getItem("AccessLevel");
+    if (check_start=="$file"+9089+6912+"$$$$") {
+      Access_Check.Result = "..?..0"
+    } else {
+      Access_Check.Result = "??/ADM.IN/]]]Needed"
+    }
+  }
+}
