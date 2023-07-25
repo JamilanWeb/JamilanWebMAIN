2a815747cc3503d60e2cde70aa8cb2e19042dc53
Jamison Smith <102773780+Jamison-smith@users.noreply.github.com>
2023-07-25 16:55:54 -0700

    Update AdScript.js

--- Games/AdScript.js
+++ Games/AdScript.js
@@ -1,1 +1,8 @@
+var exit = function(ID) {
+  document.getElementById("AdBox"+ID).remove()
+}
+
+var info = function(AdID) {
+  document.getElementById("INFOCONTAINER").removeAttribute("hidden")
+}
 
